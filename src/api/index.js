5dc3bf574479e7345dda6bc14899b6de277f8454
Aishwarya <apm2024@gmail.com>
2021-10-20 07:46:02 +0530

    final commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,31 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const api = 'API-KEY'
-const url = 'https://newsapi.org/v2';
+const api = '020124d7d46d4cc1aaa2cba748b5555e'
+const url = 'https://newsapi.org/v2'
 
 export const fetchSources = async () => {
-    const requestSource = `sources?apiKey=`;
-    try {
-        const { data: { sources } } = await axios.get(`${url}/${requestSource}${api}`);
+  const requestSource = `sources?apiKey=`
+  try {
+    const {
+      data: { sources },
+    } = await axios.get(`${url}/${requestSource}${api}`)
 
-        return sources.map((source) => source);
-    } catch (error) {
-        return error;
-    }
-};
+    return sources.map((source) => source)
+  } catch (error) {
+    return error
+  }
+}
 
 export const fetchNews = async (request) => {
-    try {
-        const { data: { articles } } = await axios.get(`${url}/${request.category}?${request.query}&pageSize=${request.results}&apiKey=${api}`);
+  try {
+    const {
+      data: { articles },
+    } = await axios.get(
+      `${url}/${request.category}?${request.query}&pageSize=${request.results}&apiKey=${api}`
+    )
 
-        return articles.map((article) => (article));
-    } catch (error) {
-        return error;
-    }
-};
+    return articles.map((article) => article)
+  } catch (error) {
+    return error
+  }
+}
